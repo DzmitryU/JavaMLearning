f5930bd432a085296f4d544365e4c7c5acaa225d
DzmitryU <dimafromgrodno@yandex.ru>
2014-04-23 18:16:11 +0300

    Added dependencies

--- Learning/src/Main.java
+++ Learning/src/Main.java
@@ -1,6 +1,15 @@
+import net.sf.javaml.core.Dataset;
+import net.sf.javaml.tools.data.FileHandler;
+
+import java.io.File;
+import java.io.IOException;
+
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        Dataset data = FileHandler.loadDataset(new File("train.csv"), ",");
+        System.out.println(data);
+
     }
 }
