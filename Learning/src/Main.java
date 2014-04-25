8a53a35114a9686597bd998d2ba7ded5358ff74f
DzmitryU <dimafromgrodno@yandex.ru>
2014-04-25 19:58:49 +0300

    Added csv reading

--- Learning/src/Main.java
+++ Learning/src/Main.java
@@ -1,15 +1,47 @@
 import net.sf.javaml.core.Dataset;
 import net.sf.javaml.tools.data.FileHandler;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Dataset data = FileHandler.loadDataset(new File("train.csv"), ",");
-        System.out.println(data);
+        String csvFile = "train.csv";
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
 
+        try {
+
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+
+                // use comma as separator
+                String[] country = line.split(cvsSplitBy);
+
+                System.out.println(country[41]);
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        System.out.println("Done");
     }
+        //Dataset data = FileHandler.loadDataset(new File("train.csv"), ",");
+        //System.out.println(data);
+
+    //}
 }
