7b19616606f4aafa8532524ecea385bb162cfd69
Dzmitry Ulezla <dulezla@gmail.com>
2014-04-30 15:34:44 +0300

    Added class for person

--- Learning/src/Person.java
+++ Learning/src/Person.java
@@ -1,3 +1,27 @@
+import net.sf.javaml.core.Instance;
+import net.sf.javaml.core.SparseInstance;
+
+import java.util.HashMap;
+import java.util.Set;
+
 public class Person {
 
+    public Person(Integer initialClass) {
+        classValue = initialClass;
+        attributes = new HashMap<Integer, Double>();
+    }
+
+    public Instance getInstance() {
+        Instance instance = new SparseInstance(attributes.size());
+        instance.putAll(attributes);
+        instance.setClassValue(classValue);
+        return instance;
+    }
+
+    public void addAttribute(Integer attributeIndex, Double attributeValue) {
+        attributes.put(attributeIndex, attributeValue);
+    }
+
+    private Integer classValue;
+    private HashMap<Integer, Double> attributes;
 }
